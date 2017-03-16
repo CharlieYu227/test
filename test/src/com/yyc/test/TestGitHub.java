8e1e54635eb43c21b3e562fd2859f13f33e2c1e2
CharlieYu <yyc227@hotmail.com>
2017-03-16 14:36:42 +0800

    test git commit operation

--- test/src/com/yyc/test/TestGitHub.java
+++ test/src/com/yyc/test/TestGitHub.java
@@ -8,6 +8,7 @@ public class TestGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test gitHub...");
+		System.out.println("test gitHub commit operation...");
 	}
 
 }
